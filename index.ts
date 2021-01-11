f3edf91fb94ad6d51757263d3de64ed02e1790ca
YukiOchiai <xdi.grand.you.winner@gmail.com>
2021-01-12 00:33:31 +0900

    add observer main view change 'clock','search'

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
-import {app,BrowserWindow} from 'electron';
-import {worker} from "cluster";
+import {app, BrowserWindow} from 'electron';
 
-
-let win: BrowserWindow | null = null;
+const windows: Array<BrowserWindow> = [];
 let isObserverActive: boolean = true;
-let apiData: string = "";
+let stateData: string = "";
+let anyData: string = "";
 
-function createWindow () {
-    win = new BrowserWindow({
+function setupWindow(): BrowserWindow{
+    return new BrowserWindow({
         width: 1920,
         height: 1080,
         kiosk: true,
@@ -17,27 +16,40 @@ function createWindow () {
             nodeIntegration: true
         }
     });
-    win.loadFile('clock/index.html').catch(error => console.log(error));
-    win.webContents.openDevTools();
-    win.on('closed', () => {
-        win = null;
-    });
+}
+
+function initView() {
+    const win = setupWindow();
+    windows.push(win);
+    windows[0].loadFile('clock/index.html').catch(error => console.log(error));
     openThread();
 }
 
 function openThread(){
     let workerFarm = require('electron-worker-farm'),
-        workers = workerFarm(require.resolve('./callApiThread')),
-        ret = 0
-
-    const callbackFunc = (string: string) => {
-        console.log(string);
+        workers = workerFarm(require.resolve('./callApiThread'))
+    const callbackFunc = (state: string,any: string) => {
+        console.log(state);
+        windows.forEach(win => {
+            console.log(win.accessibleTitle);
+        })
+        if(stateData !== state || anyData !== any){
+            if(state === '時計') {
+                windows[0].loadFile('clock/index.html').catch(error => console.log(error));
+            } else if(state === '検索'){
+                windows[0].loadURL('https://www.google.com/search?q='+any).catch(error => console.log(error));
+            }
+        }
+        stateData = state;
+        anyData = any;
         workers(callbackFunc);
     }
     workers(callbackFunc);
 }
+
+
 //開いた時
-app.on('ready', createWindow);
+app.on('ready', initView);
 
 //windowがcloseされた時
 app.on('window-all-closed', () => {
@@ -49,8 +61,8 @@ app.on('window-all-closed', () => {
 
 //
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
+    if (windows.length === 0) {
+        initView();
     }
 });
 
