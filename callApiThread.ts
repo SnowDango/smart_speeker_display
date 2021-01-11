e048aa3c691dda439b5da2687087b0489017f9e4
SnowDango <48125577+SnowDango@users.noreply.github.com>
2021-01-12 00:46:27 +0900

    Revert "Merge pull request #8 from SnowDango/observer-state"
    This reverts commit 4d74e5245aa8c00228f2864fa42df5c3a9938323.

--- callApiThread.ts
+++ callApiThread.ts
@@ -2,18 +2,13 @@ import {AxiosResponse} from "axios";
 
 const axios = require('axios');
 
-function delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-}
-
-module.exports = async function (callback: (state: string,any: string) => void) {
+module.exports = async function (callback: (string: string) => void) {
     console.log ("*** 開始 ***")
-    await delay(1000);
     await axios.get('https://asia-northeast1-smartdispserver.cloudfunctions.net/SDState/get')
         .then(function (response: AxiosResponse) {
-            callback(response.data.state,response.data.any);
+            callback(response.data.state);
             console.log ("*** 終了 ***")
         }).catch( function (error: Error) {
-            callback(error.message,"");
+            callback(error.message);
         });
 }
--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-import {app, BrowserWindow} from 'electron';
+import {app,BrowserWindow} from 'electron';
+import {worker} from "cluster";
 
-const windows: Array<BrowserWindow> = [];
+
+let win: BrowserWindow | null = null;
 let isObserverActive: boolean = true;
-let stateData: string = "";
-let anyData: string = "";
+let apiData: string = "";
 
-function setupWindow(): BrowserWindow{
-    return new BrowserWindow({
+function createWindow () {
+    win = new BrowserWindow({
         width: 1920,
         height: 1080,
         kiosk: true,
@@ -16,40 +17,27 @@ function setupWindow(): BrowserWindow{
             nodeIntegration: true
         }
     });
-}
-
-function initView() {
-    const win = setupWindow();
-    windows.push(win);
-    windows[0].loadFile('clock/index.html').catch(error => console.log(error));
+    win.loadFile('clock/index.html').catch(error => console.log(error));
+    win.webContents.openDevTools();
+    win.on('closed', () => {
+        win = null;
+    });
     openThread();
 }
 
 function openThread(){
-    const workerFarm = require('electron-worker-farm')
-    const workers = workerFarm(require.resolve('./callApiThread'))
-    const callbackFunc = (state: string,any: string) => {
-        console.log(state);
-        windows.forEach(win => {
-            console.log(win.accessibleTitle);
-        })
-        if(stateData !== state || anyData !== any){
-            if(state === '時計') {
-                windows[0].loadFile('clock/index.html').catch(error => console.log(error));
-            } else if(state === '検索'){
-                windows[0].loadURL('https://www.google.com/search?q='+any).catch(error => console.log(error));
-            }
-        }
-        stateData = state;
-        anyData = any;
+    let workerFarm = require('electron-worker-farm'),
+        workers = workerFarm(require.resolve('./callApiThread')),
+        ret = 0
+
+    const callbackFunc = (string: string) => {
+        console.log(string);
         workers(callbackFunc);
     }
     workers(callbackFunc);
 }
-
-
 //開いた時
-app.on('ready', initView);
+app.on('ready', createWindow);
 
 //windowがcloseされた時
 app.on('window-all-closed', () => {
@@ -61,8 +49,8 @@ app.on('window-all-closed', () => {
 
 //
 app.on('activate', () => {
-    if (windows.length === 0) {
-        initView();
+    if (win === null) {
+        createWindow();
     }
 });
 
